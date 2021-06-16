5a6bc38d2feb7ee7cda8ec023f92d91c1f5d4958
DrCarlArrington <51341955+DrCarlArrington@users.noreply.github.com>
2021-06-16 13:59:59 -0400

    Add files via upload

---
+++ cryptodemo.cpp
@@ -0,0 +1,66 @@
+/******************************************************************************
+This program will allow for users to tell how much their money is worth
+in some cryptocurrencies
+*******************************************************************************/
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int choice;         // variable holds our menu choice
+    double money;       // variable to hold our money
+    double converted;   // variable to hold the converted money
+    
+    // use cout statements to display the menu
+    cout << "Select from one of the following cryptocurrencies: " << endl;
+    cout << "\t 1. Bitcoin (BTC)" << endl;
+    cout << "\t 2. Ethereum (ETH)" << endl;
+    cout << "\t 3. Dogecoin (DOGE)" << endl;
+    cout << "\t 4. Tether (USTC)" << endl;
+    cout << "Enter your choice (1, 2, 3, or 4): ";
+    cin >> choice;
+    
+    // validate so that menu choices are restricted to 1 - 4
+    while(choice < 1 or choice > 4)
+    {
+        // display an error and then re-display the menu
+        cout << endl <<"ERROR: INVALID MENU CHOICE. TRY AGAIN" << endl << endl;
+        cout << "Select from one of the following cryptocurrencies: " << endl;
+        cout << "\t 1. Bitcoin (BTC)" << endl;
+        cout << "\t 2. Ethereum (ETH)" << endl;
+        cout << "\t 3. Dogecoin (DOGE)" << endl;
+        cout << "\t 4. Tether (USTC)" << endl;
+        cout << "Enter your choice (1, 2, 3, or 4): ";
+        cin >> choice;
+    }
+    
+    // after we have validated the menu choice, get the input of our money
+    cout << endl << "Enter an amount of money (enter at least $0.00): $";
+    cin >> money;
+    
+    // validate the input of the money
+    while(money < 0)
+    {
+        // display an ERROR
+        cout << endl <<"ERROR: INVALID AMOUNT OF MONEY ENTERED. TRY AGAIN." << endl;
+        cout << endl << "Enter an amount of money (enter at least $0.00): $";
+        cin >> money;
+    }
+    cout << setprecision(2) << showpoint << fixed;
+    cout << endl << "$" << money << " USD = ";
+    cout << setprecision(6) << showpoint << fixed;
+    // use if / else if to calculate the converted money
+    if(choice == 1)
+        cout << (money * (1 / 38837.27)) << " BTC";
+    else if(choice == 2)
+        cout << (money * (1 / 2410.91)) << " ETH";
+    else if(choice == 3)
+        cout << (money * (1 / 0.32)) << " DOGE";
+    else
+        cout << (money * (1 / 1.0004)) << " USTC";
+    
+	cout << endl;
+    system("pause");
+}
+
